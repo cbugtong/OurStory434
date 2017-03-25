ec35a697ff296cd02d2a2767047b4998c9f033a9
Christopher Bugtong <cbugtong@gmail.com>
2017-03-25 17:06:14 -0400

    Building Framework (1)
    Making some camera functionality and created an Event class

---
+++ OurStory/app/src/main/java/com/example/neelimapradhan/ourstory/Event.java
@@ -0,0 +1,39 @@
+package com.example.neelimapradhan.ourstory;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Chris on 3/25/2017.
+ *
+ * This class represents events as a collection of data
+ */
+
+public class Event {
+    protected int userid;
+    protected String title, description;
+    protected List hashtags;
+    protected Time startTime, endTime;
+    protected Date startDate, endDate;
+    protected Location location;
+    protected Bitmap image;
+
+    Event(int userid, String title, String description, List<String> hashtags,
+          Time startTime, Time endTime, Date startDate, Date endDate, Location location,
+          Bitmap image) {
+
+        this.userid = userid;
+        this.title = title;
+        this.description = description;
+        this.hashtags = hashtags;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.image = image;
+    }
+}
