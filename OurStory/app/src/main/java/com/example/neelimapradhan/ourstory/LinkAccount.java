c3096b33f2bffa95113a8ce92fca4f162559cfa9
Neelima Pradhan <neelimapradhan@wireless-10-105-138-15.umd.edu>
2017-04-05 18:09:32 -0400

    added link activity page

---
+++ OurStory/app/src/main/java/com/example/neelimapradhan/ourstory/LinkAccount.java
@@ -0,0 +1,13 @@
+package com.example.neelimapradhan.ourstory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinkAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link_account);
+    }
+}
